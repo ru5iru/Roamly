9833c58bd3df5c48af70eb6c80943dc2cd6f7f44
harijayk <2020cs088@stu.ucsc.cmb.ac.lk>
2023-08-08 13:23:07 +0530

    adding env file

--- server/connect.js
+++ server/connect.js
@@ -1,16 +1,17 @@
 import pkg from 'pg';
 const { Pool } = pkg;
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables from .env file
 
 const dbConfig = {
-  host: "localhost",
-  user: "postgres",
-  password: "harini",
-  database: "Roamly",
-  port: 5432, // Default PostgreSQL port
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT, 10), // Convert port to an integer
 };
 
 const db = new Pool(dbConfig);
 
 export { db };
-
-
