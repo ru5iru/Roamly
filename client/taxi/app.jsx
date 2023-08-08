267deae4e4c3fe40a2833aff1830295073175a6d
Maleesha Manage <87364623+MaleeshaMadhuhansani99@users.noreply.github.com>
2023-08-08 20:28:28 +0530

    Add files via upload

---
+++ client/taxi/app.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div id="app">
+      <h1>Taxi Services in Belihuloya</h1>
+      <ul>
+        <li>Prasad's Tuktuk</li>
+        <li>Smart Cabs</li>
+        <li>Waterloo Taxi</li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
