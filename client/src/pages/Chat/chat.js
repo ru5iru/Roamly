538781e7e987f57e289518c3991f333ed6585619
Maleesha Manage <87364623+MaleeshaMadhuhansani99@users.noreply.github.com>
2023-08-13 17:23:41 +0530

    Create chat.js

---
+++ client/src/pages/Chat/chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatApp from './ChatApp';
+
+function chat() {
+  return (
+    <div className="chat">
+      <ChatApp />
+    </div>
+  );
+}
+
+export default chat;
