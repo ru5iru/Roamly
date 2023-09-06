3b95133f71fe2dfb94cfa8715a39fb0fc20fb5b7
Rusiru Rathmina <rusirurathminarr@gmail.com>
2023-09-06 18:02:17 +0530

    logout ui

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -24,6 +24,7 @@ const Navbar = () => {
     };
 
     const [searchText, setSearchText] = useState('');
+    const [userMenuOpen, setUserMenuOpen] = useState(false);
 
     const searchKeyClick = () => {
         document.getElementById("search-bar").classList.toggle('show-search');
@@ -48,7 +49,7 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <div className="left">
-                <img src={logo} alt="" className="logo" onClick={navigateFeed} />
+                <img src={logo} alt="" className="logo navimg" onClick={navigateFeed} />
             </div>
             <div className="right">
                 {/* < Search_component /> */}
@@ -63,20 +64,57 @@ const Navbar = () => {
                                 onChange={handleInputChange}
                                 id="searchId"
                             />
-                            <img src={Search_blue} type="submit" className="search-button-b" onClick={handleSearch} />
+                            <img src={Search_blue} type="submit" className="search-button-b navimg" onClick={handleSearch} />
                         </div>
                         <img
                             src={Search}
-                            className="search-button"
+                            className="search-button navimg"
                             id="search-button"
                             onClick={searchKeyClick}
                             alt="Search Icon"
                         />
                     </form>
                 </div>
-                <img src={Chat} alt="" />
-                <img src={Notification} alt="" />
-                <img src={currentUser.profile_pic} alt="" className="usr_pic" onClick={handleProfileClick} />
+                <img className='navimg' src={Chat} alt="" />
+                <img className='navimg' src={Notification} alt="" />
+                <img src={currentUser.profile_pic} alt="" className="usr_pic navimg" onClick={() => setUserMenuOpen(!userMenuOpen)} />
+                {userMenuOpen && (
+               <div className="userMenu">
+                  <div className="userNav" onClick={handleProfileClick}>
+                     <div className="userImg">
+                        <img src={currentUser.profile_pic} alt="" />
+                     </div>
+                     <div className="userName">
+                        {currentUser.firstname} {currentUser.lastname}
+                     </div>
+                  </div>
+                  <div className="itemContainer">
+                     <div className="itemIcon">
+                        <img
+                           src="https://img.icons8.com/ios-glyphs/60/aaaaaa/exit.png"
+                           alt="exit"
+                        />
+                     </div>
+                     <div className="itemLabel">Logout</div>
+                  </div>
+                  <div className="itemContainer">
+                     <div className="itemIcon">
+                        <img
+                           src="https://img.icons8.com/ios-glyphs/60/aaaaaa/help.png"
+                           alt="exit"
+                        />
+                     </div>
+                     <div className="itemLabel">FAQ</div>
+                  </div>
+
+                  <img
+                     className="closeImg"
+                     onClick={() => setUserMenuOpen(!userMenuOpen)}
+                     src="https://img.icons8.com/material-outlined/96/777777/cancel--v1.png"
+                     alt="cancel--v1"
+                  />
+               </div>
+            )}
             </div>
         </div>
     );
