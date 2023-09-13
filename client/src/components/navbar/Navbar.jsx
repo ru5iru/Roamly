a6613b0cf32087c6a07571d777acbd2b485fc240
Rusiru Rathmina <rusirurathminarr@gmail.com>
2023-09-14 00:30:20 +0530

    badge css updated

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 import "./navbar.scss";
 import Chat from "../../assets/Chat.png";
@@ -13,72 +13,85 @@ import Search_component from "../search/search.jsx";
 import { AuthContext } from "../../context/authContext";
 
 const Navbar = () => {
+   const { currentUser } = useContext(AuthContext);
 
-    const { currentUser } = useContext(AuthContext);
+   const navigate = useNavigate(); // Initialize useNavigate
 
-    const navigate = useNavigate(); // Initialize useNavigate
+   const handleProfileClick = () => {
+      // Navigate to the home page when the Home menu item is clicked
+      navigate("/profile/" + currentUser.user_id);
+   };
 
-    const handleProfileClick = () => {
-        // Navigate to the home page when the Home menu item is clicked
-        navigate("/profile/" + currentUser.user_id);
-    };
+   const [searchText, setSearchText] = useState("");
+   const [userMenuOpen, setUserMenuOpen] = useState(false);
 
-    const [searchText, setSearchText] = useState('');
-    const [userMenuOpen, setUserMenuOpen] = useState(false);
+   const searchKeyClick = () => {
+      document.getElementById("search-bar").classList.toggle("show-search");
+   };
 
-    const searchKeyClick = () => {
-        document.getElementById("search-bar").classList.toggle('show-search');
-    }
+   const handleInputChange = (e) => {
+      setSearchText(e.target.value);
+   };
 
-    const handleInputChange = (e) => {
-        setSearchText(e.target.value);
-    };
+   const navigateFeed = () => {
+      navigate("/feed");
+   };
 
-    const navigateFeed = () => {
-        navigate("/feed");
-    };
+   const handleSearch = () => {
+      // Your search logic here using the searchText
+      console.log("Searching for:", searchText);
 
-    const handleSearch = () => {
-        // Your search logic here using the searchText
-        console.log('Searching for:', searchText);
+      // Redirect to another page with the search query as a parameter
+      navigate(`/explore?query=${searchText}`);
+   };
 
-        // Redirect to another page with the search query as a parameter
-        navigate(`/explore?query=${searchText}`);
-    };
-
-    return (
-        <div className="navbar">
-            <div className="left">
-                <img src={logo} alt="" className="logo navimg" onClick={navigateFeed} />
+   return (
+      <div className="navbar">
+         <div className="left">
+            <img
+               src={logo}
+               alt="Roamly Logo"
+               className="logo navimg"
+               onClick={navigateFeed}
+            />
+         </div>
+         <div className="right">
+            <div className="search-container">
+               <form className="search" id="search-bar">
+                  <div className="display-flex-row height-100 align-items-center padding-left-3">
+                     <input
+                        type="text"
+                        placeholder="Search"
+                        className="search-input"
+                        value={searchText}
+                        onChange={handleInputChange}
+                        id="searchId"
+                     />
+                     <img
+                        src={Search_blue}
+                        type="submit"
+                        className="search-button-b navimg"
+                        onClick={handleSearch}
+                     />
+                  </div>
+                  <img
+                     src={Search}
+                     className="search-button navimg"
+                     id="search-button"
+                     onClick={searchKeyClick}
+                     alt="Search Icon"
+                  />
+               </form>
             </div>
-            <div className="right">
-                {/* < Search_component /> */}
-                <div className="search-container">
-                    <form className="search" id="search-bar">
-                        <div className="display-flex-row height-100 align-items-center padding-left-3">
-                            <input
-                                type="text"
-                                placeholder="Search"
-                                className="search-input"
-                                value={searchText}
-                                onChange={handleInputChange}
-                                id="searchId"
-                            />
-                            <img src={Search_blue} type="submit" className="search-button-b navimg" onClick={handleSearch} />
-                        </div>
-                        <img
-                            src={Search}
-                            className="search-button navimg"
-                            id="search-button"
-                            onClick={searchKeyClick}
-                            alt="Search Icon"
-                        />
-                    </form>
-                </div>
-                <img className='navimg' src={Chat} alt="" />
-                <img className='navimg' src={Notification} alt="" />
-                <img src={currentUser.profile_pic} alt="" className="usr_pic navimg" onClick={() => setUserMenuOpen(!userMenuOpen)} />
-                {userMenuOpen && (
+            <img className="navimg" src={Chat} alt="" />
+            <img className="navimg" src={Notification} alt="" />
+            <img
+               src={currentUser.profile_pic}
+               alt=""
+               className="usr_pic navimg"
+               onClick={() => setUserMenuOpen(!userMenuOpen)}
+            />
+            {userMenuOpen && (
                <div className="userMenu">
                   <div className="userNav" onClick={handleProfileClick}>
                      <div className="userImg">
@@ -115,9 +128,9 @@ const Navbar = () => {
                   />
                </div>
             )}
-            </div>
-        </div>
-    );
+         </div>
+      </div>
+   );
 };
 
 export default Navbar;
