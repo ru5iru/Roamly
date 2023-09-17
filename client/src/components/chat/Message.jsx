3e3c9a2937d49d8c38d676cbc1822865ef8eaabc
Maleesha Madhuhansani <maleemapalagama@gmail.com>
2023-09-17 17:34:39 +0530

    add date under the content

--- client/src/components/chat/Message.jsx
+++ client/src/components/chat/Message.jsx
@@ -14,14 +14,25 @@ const Message = ({message}) => {
     ref.current?.scrollIntoView({behavior:"smooth"})
   }, [message])
 
+  const messageDate = message.date.toDate();
+  const formattedDateTime = messageDate.toLocaleString(undefined, {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+
+
   return (
     <div ref={ref} className={`message ${message.senderId === currentUser.email ? "owner" : "other"} `} >
-      <div className="messageInfo">
-        <span>{message.Date}</span>
-      </div>
+      
       <div className="messageContent">
         <p>{message.text}</p>
         {message.img && <img src={message.img} alt="" />}
+        <div className="messageInfo">
+        <span className="messageTime">{formattedDateTime}</span> {/* Display the formatted date and time */}
+      </div>
       </div>
       
     </div>
