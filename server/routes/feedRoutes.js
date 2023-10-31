870b6e8a5598ee2f9a6a13b470dc8858d27a72cd
Maleesha Madhuhansani <maleemapalagama@gmail.com>
2023-10-31 16:37:06 +0530

    unique files of chat

--- server/routes/feedRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { getFeed } from "../controllers/feedController.js";
-
-const router = express.Router();
-
-router.get("/", getFeed);
-
-export default router;
--- server/routes/notificationRoutes.js
+++
@@ -1,9 +0,0 @@
-// import express from 'express';
-// import { getNotifications } from '../controllers/notificationController.js';
-
-// const router = express.Router();
-
-// router.get('/notifications', getNotifications);
-
-// export default router;
-
