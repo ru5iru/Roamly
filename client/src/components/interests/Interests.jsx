2532377ba8333b202f2cf84f55dac0a600e6be98
Rusiru Rathmina <rusirurathminarr@gmail.com>
2023-10-31 10:25:02 +0530

    changes

--- client/src/components/interests/Interests.jsx
+++ client/src/components/interests/Interests.jsx
@@ -1,11 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./interests.scss";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
 
 const Interests = ({ userID }) => {
    const [interestData, setInterestData] = useState([]);
    const [error, setError] = useState(null);
+   const { currentUser } = useContext(AuthContext);
 
    const navigate = useNavigate();
 
@@ -26,21 +28,31 @@ const Interests = ({ userID }) => {
       navigate("/markinterests");
    };
 
+   console.log(userID, currentUser.user_id)
+
    return (
       <div className="interests">
          <div className="interestsHeadContainer">
             <div className="interestsHead">Interested In</div>
-            <div className="seeMore" onClick={handleEditClick}>Edit</div>
+            {userID == currentUser.user_id ? (
+               <div className="seeMore" onClick={handleEditClick}>
+                  Edit
+               </div>
+            ) : (
+               ""
+            )}
          </div>
-         
+
          <div className="interestContainer">
-            {interestData.length > 0
-               ? interestData.map((interest) => (
-                    <div className="interest" key={interest.interest_id}>
-                       {interest.interest_name}
-                    </div>
-                 ))
-               : <div className="empty">Nothing to show. :{`(`}</div>}
+            {interestData.length > 0 ? (
+               interestData.map((interest) => (
+                  <div className="interest" key={interest.interest_id}>
+                     {interest.interest_name}
+                  </div>
+               ))
+            ) : (
+               <div className="empty">Nothing to show. :{`(`}</div>
+            )}
          </div>
       </div>
    );
